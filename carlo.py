c51a6767e2db90f7fb6b5689a3446bd8f288d3c7
BoppreH <boppreh@gmail.com>
2021-09-22 23:00:19 +0200

    Use naive histogram

--- carlo.py
+++ carlo.py
@@ -1,25 +1,41 @@
 import time, math
 
+class Digest:
+    def __init__(self, n_bins=100):
+        self.results = []
+        self.min_found, self.max_found = float('inf'), float('-inf')
+        self.mean = None
+        self.n = 0
+
+    def update(self, value):
+        self.n += 1
+        self.results.append(value)
+        self.min_found = min(self.min_found, value)
+        self.max_found = max(self.max_found, value)
+        self.mean = value if self.mean is None else (self.mean * self.n + value) / (self.n + 1)
+
+    def describe(self, n_bins):
+        if not self.results:
+            return {}
+
+        s = sorted(self.results)
+        prev_bin = 0
+        bin_width = (s[-1] - s[0]) / n_bins
+        bins = [0 for i in range(n_bins+1)]
+        for n in s:
+            bins[int((n - s[0]) / bin_width)] += 1 
+        return self.min_found, self.max_found, self.mean, {s[0] + bin_width * i: bins[i] / len(s) for i in range(n_bins)}
+
 def plot_sample(fn, n):
     from threading import Thread, Lock
-    from tdigest import TDigest
-    digest = TDigest()
-    min_found = max_found = fn()
     stop = False
-    digest_lock = Lock()
+    digest = Digest()
 
     def run_samples():
-        nonlocal min_found, max_found
-
-        last_time = time.time()
-        
         for i in range(n-1):
             if stop:
                 return
-            result = fn()
-            with digest_lock:
-                digest.update(result)
-                min_found, _, max_found = sorted([min_found, result, max_found])
+            digest.update(fn())
 
     Thread(target=run_samples).start()
 
@@ -31,25 +47,15 @@ def plot_sample(fn, n):
     plt.show(block=False)
     while True:
         subplot.clear()
-        n_values = 100
-        with digest_lock:
-            values = [min_found + (max_found - min_found) / n_values * i for i in range(n_values)]
-            # Sorted because it often returns percentiles that are decreasing.
-            #percentiles = sorted([min_found] + [digest.percentile(i) for i in range(1, 100)] + [max_found])
-            cdf = [digest.cdf(value) for value in values]
-        print(values[0], cdf[0])
-        pdf = [cdf[0]] + [cdf[i] - cdf[i-1] for i in range(1, len(cdf))]
-        #print(values)
-        #print(pdf)
-        #print(cdf)
-        subplot.plot(values, pdf)
+        min_found, max_found, mean, pdf = digest.describe(100)
+        subplot.plot(pdf.keys(), pdf.values())
         format_number = lambda n: f'{float(f"{n:.4g}"):g}'
-        plt.xlabel('Return')
+        plt.xlabel('Result')
         plt.ylabel('Probability')
         plt.ylim([0, 1])
-        plt.xlim([min_found, max_found])
+        #plt.xlim([min_found, max_found])
         subplot.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
-        subplot.title.set_text(f'Min: {format_number(min_found)} - Mean: {format_number(digest.percentile(50))} - Max: {format_number(max_found)}')
+        subplot.title.set_text(f'Min: {format_number(min_found)} - Mean: {format_number(mean)} - Max: {format_number(max_found)}')
         fig.canvas.draw_idle() # Super important, otherwise the window freezes.
         try:
             fig.canvas.flush_events()
@@ -71,6 +77,6 @@ if __name__ == '__main__':
     results = []
     for i in range(1000):
         results.append(max(0.5, random.random()))
-    #plot_sample(lambda: time.sleep(0.01) or flip_or_bust(), n=1000)
-    plot_sample(lambda: max(0.5, random.random()), n=1000)
+    plot_sample(lambda: time.sleep(0.01) or flip_or_bust(), n=10000)
+    #plot_sample(lambda: (time.sleep(0.001), max(0.5, random.random()))[1], n=1000)
     #plot_sample(lambda: time.sleep(0.01) or random.choice([1, 2]), n=1000)
