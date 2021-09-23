50df9bd8db8da17297a91c3c31485d701f372203
BoppreH <boppreh@gmail.com>
2021-09-23 18:34:25 +0200

    Swap threading for multiprocess

--- carlo.py
+++ carlo.py
@@ -1,4 +1,10 @@
-import time, math
+import multiprocessing
+from matplotlib import pyplot as plt
+import matplotlib.ticker as mtick
+from collections import namedtuple
+import queue
+
+Snapshot = namedtuple('Snapshot', 'n min max mean bins_width pdf')
 
 class Digest:
     def __init__(self, n_bins, seed_value=0):
@@ -32,54 +38,54 @@ class Digest:
         #print(f'{value=}, {self.bins=}, {bin_i=}, {self.bins_width=}, {self.bins_start=}, {self.bins_end=}')
         self.bins[bin_i] += 1
 
-    def describe(self):
-        return self.min_found, self.max_found, self.mean, {self.bins_start + self.bins_width * i: bin_value / sum(self.bins) for i, bin_value in enumerate(self.bins)}
+    def get_snapshot(self):
+        return Snapshot(self.n, self.min_found, self.max_found, self.mean, self.bins_width, {self.bins_start + self.bins_width * i: bin_value / sum(self.bins) for i, bin_value in enumerate(self.bins)})
 
-def plot_sample(fn, n):
-    from threading import Thread, Lock
-    stop = False
-    digest = Digest(20, fn())
+def show_plot(snapshots_queue):
+    def redraw():
+        try:
+            snapshot = snapshots_queue.get(False)
+        except queue.Empty:
+            return
 
-    def run_samples():
-        nonlocal stop
-        for i in range(n-1):
-            if stop:
-                return
-            digest.update(fn())
-        stop = True
+        plt.bar(snapshot.pdf.keys(), snapshot.pdf.values(), width=snapshot.bins_width)
+        format_number = lambda n: f'{float(f"{n:.4g}"):g}'
+        plt.xlabel('Result')
+        plt.ylabel('Probability')
+        #plt.ylim([0, 1])
+        #plt.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
+        mode = max(snapshot.pdf.keys(), key=snapshot.pdf.__getitem__)
+        plt.title(f'Samples: {format_number(snapshot.n)} - Min: {format_number(snapshot.min)} - Mean: {format_number(snapshot.mean)} - Mode: {format_number(mode)}±{format_number(snapshot.bins_width/2)} - Max: {format_number(snapshot.max)}')
+        plt.draw()
 
-    Thread(target=run_samples).start()
-
-    from matplotlib import pyplot as plt
-    import matplotlib.ticker as mtick
     fig = plt.figure()
-    subplot = fig.add_subplot()
-    fig.canvas.draw()
+    redraw()
     plt.show(block=False)
     while True:
-        if not stop:
-            subplot.clear()
-            min_found, max_found, mean, pdf = digest.describe()
-            subplot.bar(pdf.keys(), pdf.values(), width=digest.bins_width)
-            format_number = lambda n: f'{float(f"{n:.4g}"):g}'
-            plt.xlabel('Result')
-            plt.ylabel('Probability')
-            plt.ylim([0, 1])
-            #plt.xlim([min_found, max_found])
-            subplot.yaxis.set_major_formatter(mtick.PercentFormatter(1.0))
-            mode = max(pdf.keys(), key=pdf.__getitem__)
-            subplot.title.set_text(f'Min: {format_number(min_found)} - Mean: {format_number(mean)} - Mode: {format_number(mode)}±{format_number(digest.bins_width/2)} - Max: {format_number(max_found)}')
-        fig.canvas.draw_idle() # Super important, otherwise the window freezes.
+        plt.clf()
+        redraw()
         try:
             fig.canvas.flush_events()
         except:
             break
 
-    stop = True
+def plot_sample(fn, n=1e6, n_bins=20):
+    snapshots_queue = multiprocessing.Queue()
+
+    plot_process = multiprocessing.Process(target=show_plot, args=(snapshots_queue,))
+    plot_process.start()
+
+
+    digest = Digest(n_bins=n_bins, seed_value=fn())
+    for _ in range(n-1):
+        digest.update(fn())
+        if snapshots_queue.empty():
+            snapshots_queue.put(digest.get_snapshot())
+
     return digest
 
 if __name__ == '__main__':
-    import random
+    import random, time
     def flip_or_bust():
         money = 1
         while True:
@@ -90,6 +96,6 @@ if __name__ == '__main__':
     results = []
     for i in range(1000):
         results.append(max(0.5, random.random()))
-    #plot_sample(lambda: time.sleep(0.01) or flip_or_bust(), n=10000)
-    plot_sample(lambda: (time.sleep(0.01), max(0.5, random.random()))[1], n=1000)
+    plot_sample(lambda: time.sleep(0.00001) or flip_or_bust(), n=10000)
+    #plot_sample(lambda: (None, max(0.5, random.random()))[1], n=100000000)
     #plot_sample(lambda: time.sleep(0.01) or random.choice([1, 2]), n=1000)
