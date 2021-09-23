30dbbe284deee864a0e1edb42e15cf912c35a458
BoppreH <boppreh@gmail.com>
2021-09-23 22:33:07 +0200

    Improve bin allocation

--- carlo.py
+++ carlo.py
@@ -8,18 +8,21 @@ Snapshot = namedtuple('Snapshot', 'n min max mean bins_width bins')
 
 class Digest:
     def __init__(self, n_bins, seed_value=0):
+        assert n_bins % 2 == 0, 'Number of bins must be even.'
         self.results = []
         self.min_found, self.max_found = float('inf'), float('-inf')
         self.mean = None
         self.n = 0
         self.bins_start = seed_value
-        self.bins_end = seed_value * 1.1
+        self.bins_end = seed_value
         self.n_bins = n_bins
-        self.bins = [1] + [0] * (self.n_bins - 1)
+        self.bins = [1]
+        self.is_int = isinstance(seed_value, int)
 
     @property
     def bins_width(self):
-        return (self.bins_end - self.bins_start) / len(self.bins)
+        width = (self.bins_end - self.bins_start) / self.n_bins
+        return max(1, int(width)) if self.is_int else width
     
     def update(self, value):
         self.n += 1
@@ -27,26 +30,44 @@ class Digest:
         self.max_found = max(self.max_found, value)
         self.mean = value if self.mean is None else (self.mean * self.n + value) / (self.n + 1)
 
-        while value > self.bins_end:
-            self.bins_end = self.bins_end + (self.bins_end - self.bins_start)
-            self.bins = [self.bins[i] + self.bins[i+1] for i in range(0, len(self.bins), 2)] + len(self.bins)//2 * [0]
+        if len(self.bins) == 1:
+            if value == self.bins_start:
+                self.bins[0] += 1
+                return
+
+            distance = value - self.bins_start
+            midway = self.bins_start + distance/2
+            width = abs(distance) / self.n_bins
+            if self.is_int:
+                midway = int(midway)
+                width = max(1, int(width))
+            #print(f'Initializing bins {self.bins_start=}, {value=}, {distance=} {midway=} {width=}')
+            self.bins_start = midway - self.n_bins // 2 * width
+            self.bins_end = midway + self.n_bins // 2 * width
+            self.bins = (self.n_bins - 1) * [0] + [self.bins[0]] + (self.n_bins - 1) * [0]
+            #print('Initialized bins', self.bins_start, value, self.bins_end, self.bins_width)
+
+        while value >= self.bins_end:
+            #print('Increasing end', self.bins_start, value, self.bins_end, self.bins_width)
+            self.bins_end = self.bins_start + 2 * self.n_bins * self.bins_width
+            self.bins = [self.bins[i] + self.bins[i+1] for i in range(0, self.n_bins, 2)] + self.n_bins//2 * [0]
         while value < self.bins_start:
-            self.bins_start = self.bins_start - (self.bins_end - self.bins_start)
-            self.bins = len(self.bins)//2 * [0] + [self.bins[i] + self.bins[i+1] for i in range(0, len(self.bins), 2)]
+            #print('Reducing start', self.bins_start, value, self.bins_end, self.bins_width)
+            self.bins_start = self.bins_end - 2 * self.n_bins * self.bins_width
+            self.bins = self.n_bins//2 * [0] + [self.bins[i] + self.bins[i+1] for i in range(0, self.n_bins, 2)]
         bin_i = int((value - self.bins_start) / self.bins_width)
-        assert bin_i >= 0
+        assert 0 <= bin_i < self.n_bins, (self.bins_start, value, self.bins_end, self.bins_width)
         self.bins[bin_i] += 1
 
     def get_snapshot(self):
-        return Snapshot(self.n, self.min_found, self.max_found, self.mean, self.bins_width, {self.bins_start + self.bins_width * i: bin_value / sum(self.bins) for i, bin_value in enumerate(self.bins) if bin_value})
+        return Snapshot(self.n, self.min_found, self.max_found, self.mean, self.bins_width, {self.bins_start + self.bins_width * i: bin_value for i, bin_value in enumerate(self.bins) if bin_value})
 
 def _run_plot(receiver_pipe):
-    def redraw():
-        if receiver_pipe.poll():
-            snapshot = receiver_pipe.recv()
 
+    def draw(snapshot):
         plt.clf()
-        plt.bar(snapshot.bins.keys(), snapshot.bins.values(), width=snapshot.bins_width)
+        #print(snapshot.bins)
+        plt.bar(snapshot.bins.keys(), [value / snapshot.n for value in snapshot.bins.values()], width=snapshot.bins_width)
         format_number = lambda n: f'{float(f"{n:.4g}"):g}'
         plt.xlabel('Result')
         plt.ylabel('Probability')
@@ -56,11 +77,14 @@ def _run_plot(receiver_pipe):
         plt.title(f'Samples: {format_number(snapshot.n)} - Min: {format_number(snapshot.min)} - Mean: {format_number(snapshot.mean)} - Mode: {format_number(mode)}±{format_number(snapshot.bins_width/2)} - Max: {format_number(snapshot.max)}')
         plt.draw()
 
+    snapshot = receiver_pipe.recv()
     fig = plt.figure()
-    redraw()
+    draw(snapshot)
     plt.show(block=False)
     while True:
-        redraw()
+        if receiver_pipe.poll():
+            snapshot = receiver_pipe.recv()
+        draw(snapshot)
         try:
             fig.canvas.flush_events()
         except:
